0065f3c974f93164d11c438b37831e486b9d9b21
sempr <iamsempr@gmail.com>
2022-12-22 18:57:43 +0800

    ADD: Day01

---
+++ src/Day00.kt
@@ -0,0 +1,20 @@
+import java.lang.Integer.max
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,41 @@
+import java.lang.Integer.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var mx = 0
+        var sm = 0
+        for (item in input) {
+            if (item == ""){
+                mx = max(mx,sm)
+                sm = 0
+            }else{
+                sm += item.toInt()
+            }
+        }
+        return max(sm, mx)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sm = 0
+        var nums = arrayOf(0)
+        for (item in input) {
+            if (item == ""){
+                nums += sm
+                sm = 0
+            }else{
+                sm += item.toInt()
+            }
+        }
+        nums += sm
+        nums.sort()
+        nums.reverse()
+        return nums[0]+nums[1]+nums[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     part1(input).println()
